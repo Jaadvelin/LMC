86c64721ee5794cd62da7e041a72c509156dabc9
Radgris <golden_run@hotmail.com>
2018-08-29 09:46:10 -0600

    New components
    Added Basic structure for some components

--- LMC.js
+++ LMC.js
@@ -1,3 +1,19 @@
+
+
+
+LMCCalculator = new Calculator;
+
+
+
+function getInstructions() {
+  unorderedString = document.getElementById("inputTextArea").value
+  unorderedArray = unorderedString.split("\n")
+      //console.log(unorderedArray)
+  return unorderedArray
+}
+
+
+
 function Calculator()
 {
   
@@ -8,4 +24,38 @@ Calculator.prototype.display = 0;
 Calculator.prototype.Sum = function(number)
 {
   this.display += number;
+}
+
+function InstructionCounter()
+{
+
+}
+
+InstructionCounter.prototype.currentInstruction = 0;
+
+InstructionCounter.prototype.IncreaseInstruction = function()
+{
+  this.currentInstruction ++;
+}
+
+InstructionCounter.prototype.SetInsturction = function(number)
+{
+  this.currentInstruction = number;
+}
+
+
+
+function InterruptManager()
+{
+
+
+}
+
+InterruptManager.prototype.instructionCounterCache = 0;
+InterruptManager.prototype.calculatorCache = 0;
+
+InterruptManager.prototype.CacheValues = function(Calculator, InstructionCounter)
+{
+ this.instructionCounterCache = InstructionCounter.currentInstruction;
+ this.calculatorCache = Calculator.display;
 }
