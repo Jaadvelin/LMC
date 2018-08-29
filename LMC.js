2f476f29a0aed568732651246e6df803b1f320aa
Radgris <golden_run@hotmail.com>
2018-08-29 09:47:40 -0600

    Doc
    Added Documentation and TODO in JS

--- LMC.js
+++ LMC.js
@@ -1,10 +1,10 @@
 
 
-
+//test to verify LMC calculator is working, eventually will be done with all components to initialize them, should be done inside a general function
 LMCCalculator = new Calculator;
 
 
-
+//example function to pull html textbox to parse, can and should be removed eventually
 function getInstructions() {
   unorderedString = document.getElementById("inputTextArea").value
   unorderedArray = unorderedString.split("\n")
@@ -13,7 +13,7 @@ function getInstructions() {
 }
 
 
-
+//Calculator  Component, can add, sub, and display info
 function Calculator()
 {
   
@@ -26,6 +26,7 @@ Calculator.prototype.Sum = function(number)
   this.display += number;
 }
 
+//Instruction Counter Component, can display, add ( increments of one) or directly set it's value
 function InstructionCounter()
 {
 
@@ -44,7 +45,7 @@ InstructionCounter.prototype.SetInsturction = function(number)
 }
 
 
-
+//interrupt Manager Component, needs to cache the values of the current calculator and the instruction Conter and return them aswell( inside of their components)
 function InterruptManager()
 {
 
@@ -59,3 +60,5 @@ InterruptManager.prototype.CacheValues = function(Calculator, InstructionCounter
  this.instructionCounterCache = InstructionCounter.currentInstruction;
  this.calculatorCache = Calculator.display;
 }
+
+//todo: mailbox component, IO box component, LMC component
